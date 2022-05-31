4401618bfde52f43aedf5f013277ef66c17f18bb
vannuthchhorn007 <vannuthchhorn4@gmail.com>
2022-05-31 23:15:59 +0700

    add more

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import './App.css';
-class Students extends React.Component {
-	constructor() {
-	  super();
-	  this.state = { students: [] };
-	}
-  
-	componentDidMount() {
-	  fetch('http://127.0.0.1:8000/api/students')
-		.then(response => response.json())
-		.then(json => this.setState({ students: json.data }));
-	}
-  
-	render() {
-	  return (
-		<div>
-		  <h1>Students</h1>
-		  {
-			this.state.students.length == 0
-			  ? 'Loading students...'
-			  : this.state.students.map(user => (
-				<figure key={user.id}>
-				  <figcaption>
-					{user.name}
-				  </figcaption>
-				</figure>
-			  ))
-		  }
-		</div>
-	  );
-	}
-  }
-  
-  ReactDOM.render(<Students />, document.getElementById('root'));
+class App extends React.Component {
+   
+    // Constructor 
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+   
+    // ComponentDidMount is used to
+    // execute the code 
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Pleses wait some time.... </h1> </div> ;
+   
+        return (
+        <div className = "App">
+            <h1> Fetch data from an api in react </h1>  {
+                items.map((item) => ( 
+                <ol key = { item.id } >
+                    User_Name: { item.username }, 
+                    Full_Name: { item.name }, 
+                    User_Email: { item.email } 
+                    </ol>
+                ))
+            }
+        </div>
+    );
+}
+}
+   
+export default App;
